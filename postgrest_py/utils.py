139f180d820d59e1eda5472d0f884d99b137da35
margo0ka <mkoka97@gmail.com>
2021-12-08 18:07:06 -0800

    'fixing' issue with sanitize_params in postgrest_py/utils.py

--- postgrest_py/utils.py
+++ postgrest_py/utils.py
@@ -1,7 +1,8 @@
 def sanitize_param(param: str) -> str:
-    reserved_chars = ",.:()"
-    if any(char in param for char in reserved_chars):
-        return f'%22{param}%22'
+    # Quick fix for eq no working properly 
+    # reserved_chars = ",.:()"
+    # if any(char in param for char in reserved_chars):
+    #     return f'%22{param}%22'
     return param
 
 
